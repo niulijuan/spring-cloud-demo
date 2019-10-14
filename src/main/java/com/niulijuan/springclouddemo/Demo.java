d859d5cd3d04ac3aaa6801d0be403019e5ddac69
niulijuan <niulijuan@ksjgs.com>
2019-10-14 12:01:43 +0800

    init

---
+++ src/main/java/com/niulijuan/springclouddemo/Demo.java
@@ -0,0 +1,10 @@
+package com.niulijuan.springclouddemo;
+
+/**
+ * @author niulijuan
+ * @date 2019-08-05 14:04
+ * @desc
+ */
+public class Demo {
+
+}
---
+++ src/main/java/com/niulijuan/springclouddemo/springclouddemo/SpringCloudDemoApplication.java
@@ -0,0 +1,13 @@
+package com.niulijuan.springclouddemo.springclouddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudDemoApplication.class, args);
+    }
+
+}
